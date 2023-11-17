190863f726a52709b782fed7f8628475b5239f42
Developer-Nasim <ajnasim72@gmail.coom>
2023-11-17 14:08:32 +0600

    working or not

---
+++ assets/js/fireConfig.js
@@ -0,0 +1,12 @@
+
+var firebaseConfig = {
+  apiKey: "AIzaSyCAQc2bwogFAuy_v_M-USF53VBq6AMxREA",
+  authDomain: "fakeidcheck-4d85d.firebaseapp.com",
+  databaseURL: "https://fakeidcheck-4d85d-default-rtdb.asia-southeast1.firebasedatabase.app/",
+  projectId: "fakeidcheck-4d85d",
+  storageBucket: "fakeidcheck-4d85d.appspot.com",
+  messagingSenderId: "251581115409",
+  appId: "1:251581115409:web:df8e84e1e97d3de16015b0",
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
--- assets/js/main.js
+++ assets/js/main.js
@@ -1,128 +1,104 @@
 (function($) {
   "use strict";
-  
- // menu 
-  $('.siteBar-btn').click( function (){ 
-    $('.mobile-menu').toggleClass('siteBar');
-    $('.manu-overlay').toggleClass('show');
-  }); 
 
-  
-  // mainSlider
-  function mainSlider() {
-    var BasicSlider = $(".slider-active");
-    BasicSlider.on("init", function(e, slick) {
-      var $firstAnimatingElements = $(".single-slider:first-child").find(
-        "[data-animation]"
-      );
-      doAnimations($firstAnimatingElements);
+  // fetch('https://api.ipify.org?format=json')
+  // .then(response => response.json())
+  // .then(data => console.log(data.ip));
+ 
+
+
+  fetch("https://ipinfo.io/json?token=31bb2faf09aad3").then(
+    (response) => response.json()
+  ).then(
+    (jsonResponse) => console.log(jsonResponse)
+  )
+
+      
+
+
+
+  var rollV, nameV, genderV, addressV;
+
+function readFom() {
+  rollV = document.getElementById("roll").value;
+  nameV = document.getElementById("name").value;
+  genderV = document.getElementById("gender").value;
+  addressV = document.getElementById("address").value;
+  console.log(rollV, nameV, addressV, genderV);
+}
+
+document.getElementById("insert").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .set({
+      rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
     });
-    BasicSlider.on("beforeChange", function(e, slick, currentSlide, nextSlide) {
-      var $animatingElements = $(
-        '.single-slider[data-slick-index="' + nextSlide + '"]'
-      ).find("[data-animation]");
-      doAnimations($animatingElements);
+  alert("Data Inserted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+
+document.getElementById("read").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .on("value", function (snap) {
+      document.getElementById("roll").value = snap.val().rollNo;
+      document.getElementById("name").value = snap.val().name;
+      document.getElementById("gender").value = snap.val().gender;
+      document.getElementById("address").value = snap.val().address;
     });
-    BasicSlider.slick({
-      autoplay: false,
-      autoplaySpeed: 10000,
-      dots: false,
-      fade: true,
-      arrows: true,
-      prevArrow:
-        '<button type="button" class="slick-prev"><i class="ti-shift-left"></i></button>',
-      nextArrow:
-        '<button type="button" class="slick-next"><i class="ti-shift-right"></i></button>',
-      responsive: [
-        { breakpoint: 767, settings: { dots: false, arrows: false } }
-      ]
+};
+
+document.getElementById("update").onclick = function () {
+  readFom();
+
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .update({
+      //   rollNo: rollV,
+      name: nameV,
+      gender: genderV,
+      address: addressV,
     });
+  alert("Data Update");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
+document.getElementById("delete").onclick = function () {
+  readFom();
 
-    function doAnimations(elements) {
-      var animationEndEvents =
-        "webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend";
-      elements.each(function() {
-        var $this = $(this);
-        var $animationDelay = $this.data("delay");
-        var $animationType = "animated " + $this.data("animation");
-        $this.css({
-          "animation-delay": $animationDelay,
-          "-webkit-animation-delay": $animationDelay
-        });
-        $this.addClass($animationType).one(animationEndEvents, function() {
-          $this.removeClass($animationType);
-        });
-      });
-    }
-  }
-  mainSlider();
+  firebase
+    .database()
+    .ref("student/" + rollV)
+    .remove();
+  alert("Data Deleted");
+  document.getElementById("roll").value = "";
+  document.getElementById("name").value = "";
+  document.getElementById("gender").value = "";
+  document.getElementById("address").value = "";
+};
 
 
-  // owlCarousel
-  $(".brand-active").owlCarousel({
-    loop: true,
-    margin: 30,
-    items: 6,
-    navText: [
-      '<i class="fa fa-angle-left"></i>',
-      '<i class="fa fa-angle-right"></i>'
-    ],
-    nav: false,
-    dots: false,
-    responsive: {
-      0: {
-        items: 2
-      },
-      767: {
-        items: 3
-      },
-      992: {
-        items: 6
-      }
-    }
-  });
 
-  /* magnificPopup img view */
-  $(".popup-image").magnificPopup({
-    type: "image",
-    gallery: {
-      enabled: true
-    }
-  }); 
 
-  // counter
-  $('.counter').counterUp({
-    delay: 10,
-    time: 1000
-  });
 
-  // isotop
-  $(".grid").imagesLoaded(function() {
-    // init Isotope
-    var $grid = $(".grid").isotope({
-      itemSelector: ".grid-item",
-      percentPosition: true,
-      masonry: {
-        // use outer width of grid-sizer for columnWidth
-        columnWidth: ".grid-item"
-      }
-    });
 
-    // filter items on button click
-    $(".portfolio-menu").on("click", "button", function() {
-      var filterValue = $(this).attr("data-filter");
-      $grid.isotope({ filter: filterValue });
-    });
 
-    //for menu active class
-    $(".portfolio-menu button").on("click", function(event) {
-      $(this)
-        .siblings(".active")
-        .removeClass("active");
-      $(this).addClass("active");
-      event.preventDefault();
-    });
-  });
-  
+
+
  
 })(jQuery);
